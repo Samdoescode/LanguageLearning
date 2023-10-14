fc88a40de9e84a35b0e7aa2c2fb7068a7aac859c
Sam <kennedyhine@gmail.com>
2023-10-14 15:41:35 +1100

    rust guessing game

---
+++ Rust/rpl1.1_helloWorld/main.rs
@@ -0,0 +1,4 @@
+fn main() { // define the function and name is main - main function entry point to rust program
+    println!("hello, world!"); // call marcro (not function defined by !) println and give it property "hello
+                               // world"
+}
---
+++ Rust/rpl1.2_hello_cargo/target/debug/deps/hello_cargo-7edb11be7a8d45ce.d
@@ -0,0 +1,5 @@
+C:\Users\Sam\web_projects\languageLearning\Rust\hello_cargo\target\debug\deps\hello_cargo-7edb11be7a8d45ce.rmeta: src\main.rs
+
+C:\Users\Sam\web_projects\languageLearning\Rust\hello_cargo\target\debug\deps\hello_cargo-7edb11be7a8d45ce.d: src\main.rs
+
+src\main.rs:
